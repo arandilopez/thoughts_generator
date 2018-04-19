f195118a41b6774990910dc1e1faa2f73eed18a3
Arandi Lopez <arandilopez.93@gmail.com>
2018-04-18 19:33:57 -0500

    model de millennials

---
+++ app/models/thought.rb
@@ -0,0 +1,43 @@
+class Thought
+  attr_reader :comment
+  def initialize
+    @comment = THOUGHTS.reduce('') { |carry, part| carry << part.sample + ' ' }.strip
+  end
+  THOUGHTS = [
+    [
+      'Es bonito',
+      'Me gusta',
+      'Lo mejor del mundo',
+      'Menos mal que puedo',
+      'Amo'
+    ],
+    [
+      'encerrarme a ver Netflix',
+      'convivir con mi perrito',
+      'hablar con gente que no me tire mala vibra',
+      'leer un buen libro',
+      'ignorar a todos'
+    ],
+    [
+      'y sentirme feliz',
+      'y que me valga verga el mundo',
+      'y confiar más en mi',
+      'y empoderarme de mi ser',
+      'y consentirme un poquito más'
+    ],
+    [
+      'para alejarme de',
+      'para evitar',
+      'para no contaminarme de',
+      'para cuidarme de',
+      'para no cae en'
+    ],
+    [
+      'emociones tóxicas',
+      'personas tóxicas',
+      'situaciones tóxicas',
+      'comentarios tóxicos',
+      'conversaciones tóxicas',
+    ]
+  ]
+end
